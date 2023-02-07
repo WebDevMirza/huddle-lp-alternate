d9420a4e864869c2f7818308c1492cb4d8e52fbd
WebDev Mirza <webdevmirza@gmail.com>
2023-02-07 10:04:09 +0600

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
+import "../assets/styles/basic.css";
+import { Header } from "../compo/Header";
+import { Hero } from "../compo/Hero";
+
 export const Home = () => {
   return (
     <>
       <div className="grand-holder">
-        <header></header>
-        <main></main>
+        <header>
+          <Header />
+        </header>
+        <main>
+          <section className="hero">
+            <Hero />
+          </section>
+        </main>
         <footer></footer>
       </div>
     </>
