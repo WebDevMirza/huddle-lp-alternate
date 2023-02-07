70dd5c0c13ddf641229246e89beaa11b05dc2091
WebDev Mirza <webdevmirza@gmail.com>
2023-02-07 06:15:18 +0600

    main home page of the website

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export const Home = () => {
+  return (
+    <>
+      <div className="grand-holder">
+        <header></header>
+        <main></main>
+        <footer></footer>
+      </div>
+    </>
+  );
+};
